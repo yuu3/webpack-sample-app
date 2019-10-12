2a52a50ecc6a1a2d982cdb09885ef2d1b22243d3
yuu3 <yunosuke0802@icloud.com>
2019-10-13 01:26:47 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    app :'./src/app.js',
+    // errlog: './logs/err.js'
+  },
+  output: {
+    path: path.resolve(__dirname,'dist'),
+    filename: 'app.js'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread']
+          }
+        }
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        exclude: /node_modules/,
+        use: ['file-loader'],
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html',
+      main: {
+        entry: "./app.js"
+      }
+    })
+  ]
+}
